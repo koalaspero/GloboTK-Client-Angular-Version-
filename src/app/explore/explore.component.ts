2e03c4a32de35e2bfe9a287964974b9e9339781a
Diego Eduardo Arteaga Chavez <diedarte@espol.edu.ec>
2022-01-11 00:04:56 -0500

    Globo adaptado

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import countries from './data/countries.json';
+declare var WE: any;
 
 @Component({
   selector: 'app-explore',
@@ -8,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class ExploreComponent implements OnInit {
 
   constructor() { }
+
+  title = 'json-file-read-angular';
+  public countryList:{country:string, latitude:string, longitude:string, name:string}[] = countries;
+  
   ngOnInit(): void {
-    $('body').attr("onload","initialize()");
+    
+    var earth = new WE.map('earth_div');
+    WE.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+
+    for(let i = 0; i < this.countryList.length ; i++){
+            let htmLine = "<b>"+ this.countryList[i].name + "</b><br>Artistas más escuchados<br><a href='https://spotifycharts.com/regional/"+ this.countryList[i].country.toLowerCase()+"/daily/latest'>click here</a>";
+            let latitude = this.countryList[i].latitude;
+            let longitude = this.countryList[i].longitude;
+            var marker = WE.marker([latitude, longitude]).addTo(earth);
+            marker.bindPopup(htmLine, {maxWidth: 120, closeButton: true});
+    }
+    
+
+    //var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+    
+
+    earth.setView([-1.6, -78], 3.5);
+
   }
 
 }
